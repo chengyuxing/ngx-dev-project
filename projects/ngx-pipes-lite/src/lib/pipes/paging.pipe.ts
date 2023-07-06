a091df7997a1e4f6207997d2bc67fc8f1c706596
chengyuxing <chengyuxingo@gmail.com>
2023-07-06 21:03:03 +0800

    add paging pipe

---
+++ projects/ngx-pipes-lite/src/lib/pipes/paging.pipe.ts
@@ -0,0 +1,33 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+/**
+ * Simple array data paging pipe.
+ * @usageNotes
+ * `[] | paging:page?=1:size?=10`
+ */
+@Pipe({
+  name: 'paging'
+})
+export class PagingPipe implements PipeTransform {
+  /**
+   * Simple array data paging pipe.
+   * @param value array data to be paging
+   * @param page current page
+   * @param size page size
+   */
+  transform(value: any[], page = 1, size = 10): any[] {
+    if (!value) {
+      return [];
+    }
+    if (page < 1) {
+      page = 1;
+    }
+    if (size < 1) {
+      size = 1;
+    }
+    const start = (page - 1) * size;
+    const end = page * size;
+    return value.slice(start, end);
+  }
+
+}
