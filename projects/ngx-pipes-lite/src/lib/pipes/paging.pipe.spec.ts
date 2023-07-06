a9bffd1405d675e2bbd6a62b6ae65585fd1bbce4
chengyuxing <chengyuxingo@gmail.com>
2023-07-06 21:23:52 +0800

    tests

--- projects/ngx-pipes-lite/src/lib/pipes/paging.pipe.spec.ts
+++ projects/ngx-pipes-lite/src/lib/pipes/paging.pipe.spec.ts
@@ -1,8 +1,25 @@
-import { PagingPipe } from './paging.pipe';
+import {PagingPipe} from './paging.pipe';
 
 describe('PagingPipe', () => {
-  it('create an instance', () => {
-    const pipe = new PagingPipe();
-    expect(pipe).toBeTruthy();
+  let pipe: PagingPipe;
+  beforeEach(() => {
+    pipe = new PagingPipe();
+  });
+
+  it('should default paging 1, size 10', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const result = pipe.transform(arr);
+    expect(result).toEqual(arr.slice(0, 10));
+  });
+
+  it('should paging by custom.', () => {
+    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+    const result = pipe.transform(arr, 2, 3);
+    expect(result).toEqual([4, 5, 6]);
+  });
+
+  it('should return empty array.', () => {
+    const result = pipe.transform([], 2, 3);
+    expect(result).toEqual([]);
   });
 });
